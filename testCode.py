6d16819556ffc373629c784ffa16eb07b6ee3cae
phroiland <jon.froiland@gmail.com>
2017-02-07 15:25:57 -0500

    Updates to file go to bottom of file.

--- testCode.py
+++ testCode.py
@@ -1,12 +1,14 @@
-import re \
-from pyspark.streaming import StreamingContext \
-ssc = StreamingContext(sc, 30) \
-lines = ssc.socketTextStream(localhost, 9999) \
+import re
+from pyspark.streaming import StreamingContext
+ssc = StreamingContext(sc, 30)
+lines = ssc.socketTextStream(localhost, 9999)
 wordcounts = lines.filter(lambda line: len(line) > 0) \
-.flatMap(lambda line: re.split(W+, line)) \
-.filter(lambda word: len(word) > 0) \
-.map(lambda word: (word.lower(), 1)) \
-.reduceByKey(lambda x, y: x + y) \
-wordcounts.pprint() \
-ssc.start() \
+	.flatMap(lambda line: re.split(W+, line)) \
+	.filter(lambda word: len(word) > 0) \
+	.map(lambda word: (word.lower(), 1)) \
+	.reduceByKey(lambda x, y: x + y)
+wordcounts.pprint()
+ssc.start()
 ssc.awaitTermination()
+import re
+from pyspark.streaming import StreamingContext
