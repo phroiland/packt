7d14a0dabe149aa9ea6a7d09b349ef9cd1fe35f4
phroiland <jon.froiland@gmail.com>
2017-02-07 15:23:11 -0500

    Added code to py file

---
+++ testCode.py
@@ -0,0 +1,12 @@
+import re \
+from pyspark.streaming import StreamingContext \
+ssc = StreamingContext(sc, 30) \
+lines = ssc.socketTextStream(localhost, 9999) \
+wordcounts = lines.filter(lambda line: len(line) > 0) \
+.flatMap(lambda line: re.split(W+, line)) \
+.filter(lambda word: len(word) > 0) \
+.map(lambda word: (word.lower(), 1)) \
+.reduceByKey(lambda x, y: x + y) \
+wordcounts.pprint() \
+ssc.start() \
+ssc.awaitTermination()
